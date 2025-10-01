3681ceb060789f75b1b2dd25b147fb62c2aacfd0
relente-patriciajoy <relente.patriciajoy@gmail.com>
2025-10-01 12:55:45 +0800

    add directories and files

---
+++ dbenrollment_app/templates/sidebar.php
@@ -0,0 +1,12 @@
+<ul>
+  <li><a href="modules/students/index.php">Student</a></li>
+  <li><a href="modules/instructors/index.php">Instructor</a></li>
+  <li><a href="modules/programs/index.php">Program</a></li>
+  <li><a href="modules/departments/index.php">Department</a></li>
+  <li><a href="modules/courses/index.php">Course</a></li>
+  <li><a href="modules/course_prereq/index.php">Course Prerequisite</a></li>
+  <li><a href="modules/sections/index.php">Section</a></li>
+  <li><a href="modules/terms/index.php">Term</a></li>
+  <li><a href="modules/rooms/index.php">Room</a></li>
+  <li><a href="modules/enrollments/index.php">Enrollment</a></li>
+</ul>
