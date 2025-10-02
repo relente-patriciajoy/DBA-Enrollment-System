689a1b184fac9a681759da08ae5abc0698ed376f
relente-patriciajoy <relente.patriciajoy@gmail.com>
2025-10-02 18:10:13 +0800

    add sidebar and dashboard

--- dbenrollment_app/templates/sidebar.php
+++ dbenrollment_app/templates/sidebar.php
@@ -1,12 +1,15 @@
-<ul>
-  <li><a href="modules/students/index.php">Student</a></li>
-  <li><a href="modules/instructors/index.php">Instructor</a></li>
-  <li><a href="modules/programs/index.php">Program</a></li>
-  <li><a href="modules/departments/index.php">Department</a></li>
-  <li><a href="modules/courses/index.php">Course</a></li>
-  <li><a href="modules/course_prereq/index.php">Course Prerequisite</a></li>
-  <li><a href="modules/sections/index.php">Section</a></li>
-  <li><a href="modules/terms/index.php">Term</a></li>
-  <li><a href="modules/rooms/index.php">Room</a></li>
-  <li><a href="modules/enrollments/index.php">Enrollment</a></li>
-</ul>
+<div style="width: 200px; float: left; padding: 10px; background: #f0f0f0; height: 100vh;">
+    <h3>Maintenance</h3>
+    <ul style="list-style: none; padding-left: 0;">
+        <li><a href="/modules/students/index.php">Students</a></li>
+        <li><a href="/modules/instructors/index.php">Instructors</a></li>
+        <li><a href="/modules/programs/index.php">Programs</a></li>
+        <li><a href="/modules/departments/index.php">Departments</a></li>
+        <li><a href="/modules/courses/index.php">Courses</a></li>
+        <li><a href="/modules/course_prereq/index.php">Course Prerequisites</a></li>
+        <li><a href="/modules/sections/index.php">Sections</a></li>
+        <li><a href="/modules/terms/index.php">Terms</a></li>
+        <li><a href="/modules/rooms/index.php">Rooms</a></li>
+        <li><a href="/modules/enrollments/index.php">Enrollments</a></li>
+    </ul>
+</div>
